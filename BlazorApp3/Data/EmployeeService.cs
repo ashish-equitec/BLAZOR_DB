ab0cff41c004e0a1b767505bf5257a00532cc1e7
ashish-equitec <ashish.equitec@gmail.com>
2024-01-16 10:49:33 +0530

    init

---
+++ BlazorApp3/Data/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorApp3.Models;
+using BlazorApp3.Pages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp3.Data
+{
+    public class EmployeeService
+    {
+        private readonly CRUDOPERATIONContext _dbConnection;
+
+        public EmployeeService(CRUDOPERATIONContext dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<List<EmployeeViewResult>> ExecuteStoredProcedureAsync()
+        {
+            return await _dbConnection.Procedures.EmployeeViewAsync();
+        }
+    }
+}
